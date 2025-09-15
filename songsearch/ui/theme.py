0e01876680757822dc14c70ed482acd8fef71200
Hache <thewhitetrackproject@proton.me>
2025-09-16 01:12:41 +0200

    Update theme.py with new CSS theme

--- songsearch/ui/theme.py
+++ songsearch/ui/theme.py
@@ -1,35 +1,25 @@
-from __future__ import annotations
-
-        }}
-        QHeaderView::section:horizontal:last {{
-            border-right: none;
-        }}
-            
-        }}
-        QScrollBar::add-line:vertical,
-        QScrollBar::sub-line:vertical {{
-            height: 0px;
-        }}
-        QScrollBar::sub-page:vertical,
-        QScrollBar::add-page:vertical {{
-            background: none;
-        }}
-
-        QScrollBar:horizontal {{
-
-        }}
-        QScrollBar::add-line:horizontal,
-        QScrollBar::sub-line:horizontal {{
-            width: 0px;
-        }}
-        QScrollBar::sub-page:horizontal,
-        QScrollBar::add-page:horizontal {{
-            background: none;
-        }}
-
-
-            border-radius: 6px;
-        }}
-        """
-    ).strip()
-
+THEME_CSS = """
+QHeaderView::section:horizontal:last {
+    border-right: none;
+}
+QScrollBar::add-line:vertical,
+QScrollBar::sub-line:vertical {
+    height: 0px;
+}
+QScrollBar::sub-page:vertical,
+QScrollBar::add-page:vertical {
+    background: none;
+}
+QScrollBar:horizontal {
+}
+QScrollBar::add-line:horizontal,
+QScrollBar::sub-line:horizontal {
+    width: 0px;
+}
+QScrollBar::sub-page:horizontal,
+QScrollBar::add-page:horizontal {
+    background: none;
+}
+border-radius: 6px;
+}
+""".strip()
